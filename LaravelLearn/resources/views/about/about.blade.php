4a730b89dcdebf6ee58647da8461a9c0885739c6
olek1305 <olek1305@gmail.com>
2023-07-03 11:03:33 +0200

    add switch situtation

--- LaravelLearn/resources/views/about/about.blade.php
+++ LaravelLearn/resources/views/about/about.blade.php
@@ -11,4 +11,22 @@
             </div>
         @endif
     @endforeach
+
+    @php
+        $i = 3;
+    @endphp
+
+    @switch($i)
+        @case(1)
+            <div class="alert alert-danger">Danger</div>
+        @break
+
+        @case(2)
+            <div class="alert alert-success">Success</div>
+        @break
+
+        @case(3)
+            <div class="alert alert-warning">Warning</div>
+        @break
+    @endswitch
 @endsection
