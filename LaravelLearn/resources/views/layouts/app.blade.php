77c509d5c926ae64349f2742b1e781a6dc53a469
olek1305 <olek1305@gmail.com>
2023-07-03 09:20:03 +0200

    add bootstrap, copyright

--- LaravelLearn/resources/views/layouts/app.blade.php
+++ LaravelLearn/resources/views/layouts/app.blade.php
@@ -5,11 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
     @yield('content')
+    <footer class="bg-light text-center" style="position: fixed; width: 100%; bottom: 0;">
+        <!--copyright-->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">2022 Copyright:
+            <a class="text-dark" href="#">Arcylisz</a>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
