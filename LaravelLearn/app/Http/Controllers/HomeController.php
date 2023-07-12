8eb1e63d0a7d813cb96207151561293e98094de2
olek1305 <olek1305@gmail.com>
2023-07-12 23:16:27 +0200

    example codes

--- LaravelLearn/app/Http/Controllers/HomeController.php
+++ LaravelLearn/app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Posts;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 
@@ -10,6 +10,35 @@ class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return $posts = Posts::all();
+        //THERE CODES IN COMMENT WERE IN TRAINING
+
+        // return Post::all(); // DB::table('posts)->get();
+
+        // $post = Post::FindOrFail(32);
+        // return $post->title;
+
+        // $posts = [];
+        // $posts = Post::all();
+        // return view('home')->with('posts', $posts);
+
+        // $post = new Post();
+        // $post->title = 'post 4';
+        // $post->description = 'this is a test description';
+        // $post->status = 1;
+        // $post->publish_date = date('Y-m-d');
+        // $post->user_id = 5;
+
+        // $post->save();
+        // dd('success');
+
+
+
+        // $post = Post::find(16);
+        // $post->title = 'This is a new title';
+        // $post->save();
+
+        // dd('success');
+
+        
     }
 }
