46ed398eb6b95d05b0ab346ea6fb0384e346332a
olek1305 <olek1305@gmail.com>
2023-07-22 16:26:36 +0200

    added logout and register

--- LaravelLearnVue/routes/web.php
+++ LaravelLearnVue/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +22,6 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
