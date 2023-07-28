9eb22be018864a4025b863f4f013d2a324b06fae
olek1305 <olek1305@gmail.com>
2023-07-28 10:59:16 +0200

    Tasks

--- LaravelLearnVue/app/Http/Controllers/Api/V2/TaskController.php
+++ LaravelLearnVue/app/Http/Controllers/Api/V2/TaskController.php
@@ -10,6 +10,10 @@ use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
     /**
      * Display a listing of the resource.
      */
@@ -33,7 +37,6 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        $this->authorize('view', $task);
         return TaskResource::make($task);
     }
 
