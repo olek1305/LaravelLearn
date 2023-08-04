e03b18fd8cfe836132c80e3a537c80d38e1b8e0f
olek1305 <olek1305@gmail.com>
2023-08-04 18:11:34 +0200

    fix tak api

--- LaravelLearnVue/toedoe-app/src/http/task-api.js
+++ LaravelLearnVue/toedoe-app/src/http/task-api.js
@@ -2,12 +2,12 @@ import api from "./api"
 
 const resource = "/tasks"
 
-export const allTasks = () => api.get(resource)
+export const allTasks = () => api.get(resource);
 
-export const createTask = task => api.post(resource, task)
+export const createTask = (task) => api.post(resource, task);
 
-export const updateTask = (id, task) => api.put('${resource}/${id}', task)
+export const updateTask = (id, task) => api.put(`${resource}/${id}`, task);
 
-export const removeTask = id => api.delete('${resource}/${id}')
+export const removeTask = (id) => api.delete(`${resource}/${id}`);
 
-export const completeTask = (id, task) => api.patch('${resource}/${id}/complete', task)
+export const completeTask = (id, task) => api.patch(`${resource}/${id}/complete`, task);
