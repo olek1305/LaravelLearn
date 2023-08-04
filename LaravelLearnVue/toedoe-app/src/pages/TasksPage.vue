bcc4caf66d56d9e68fc9c2caee8f6d981a608a48
olek1305 <olek1305@gmail.com>
2023-08-04 22:16:37 +0200

    add edit & udpate

--- LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
+++ LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
@@ -5,8 +5,9 @@
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
                     <NewTask @added="handleAddedTask" />
+
                     <!-- List of uncompleted tasks -->
-                    <Tasks :tasks="uncompletedTasks" />
+                    <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask" />
 
                     <!-- show toggle button -->
                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
@@ -27,7 +28,7 @@
 
 <script setup>
 import { onMounted, ref, computed } from "vue";
-import { allTasks, createTask } from "../http/task-api";
+import { allTasks, createTask, updateTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue"
 import NewTask from "../components/tasks/NewTask.vue"
 
@@ -52,5 +53,13 @@ const showCompletedTasks = ref(false)
 const handleAddedTask = async (newTask) => {
     const { data: createdTask } = await createTask(newTask)
     tasks.value.unshift(createdTask.data)
+}
+
+const handleUpdatedTask = async (task) => {
+    const { data: updatedTask } = await updateTask(task.id, {
+        name: task.name
+    })
+    const currentTask = tasks.value.find(item => item.id === task.id)
+    currentTask.name = updatedTask.data.name
 }
 </script>
