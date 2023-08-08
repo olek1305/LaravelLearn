ec8cd4a0c5b46e30e2d7077c857baab639c33f0e
olek1305 <olek1305@gmail.com>
2023-08-08 11:31:25 +0200

    add is_completed

--- LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
+++ LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
@@ -7,7 +7,7 @@
                     <NewTask @added="handleAddedTask" />
 
                     <!-- List of uncompleted tasks -->
-                    <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask" />
+                    <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask" @completed="handleCompletedTask" />
 
                     <!-- show toggle button -->
                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
@@ -28,7 +28,7 @@
 
 <script setup>
 import { onMounted, ref, computed } from "vue";
-import { allTasks, createTask, updateTask } from "../http/task-api";
+import { allTasks, createTask, updateTask, completeTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue"
 import NewTask from "../components/tasks/NewTask.vue"
 
@@ -61,5 +61,13 @@ const handleUpdatedTask = async (task) => {
     })
     const currentTask = tasks.value.find(item => item.id === task.id)
     currentTask.name = updatedTask.data.name
+}
+
+const handleCompletedTask = async (task) => {
+    const { data: updatedTask } = await completeTask(task.id, {
+        is_completed: task.is_completed
+    })
+    const currentTask = tasks.value.find(item => item.id === task.id)
+    currentTask.is_completed = updatedTask.data.is_completed
 }
 </script>
