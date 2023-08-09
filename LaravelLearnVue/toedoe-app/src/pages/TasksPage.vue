f7832fbcacc9304dd09a4d1c66eb73d1ca2b7573
olek1305 <olek1305@gmail.com>
2023-08-09 23:08:27 +0200

    test task.js

--- LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
+++ LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
@@ -30,9 +30,20 @@
 
 <script setup>
 import { onMounted, ref, computed } from "vue";
+import { storeToRefs } from "pinia"
+import { useTaskStore } from "../stores/task";
 import { allTasks, createTask, updateTask, completeTask, removeTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue"
 import NewTask from "../components/tasks/NewTask.vue"
+
+const store = useTaskStore()
+const { completedTasks, uncompletedTasks } = storeToRefs(store)
+// store.$patch({
+//     task: {
+//         name: "Hello first name",
+//         is_completed: true
+//     }
+// })
 
 const tasks = ref([])
 
@@ -41,8 +52,6 @@ onMounted(async () => {
     tasks.value = data.data
 })
 
-const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
-const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
 const showToggleCompletedBtn = computed(
     () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
 )
