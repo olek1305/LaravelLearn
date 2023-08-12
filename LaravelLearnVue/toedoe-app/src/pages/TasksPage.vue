c7e794ea9740eafa387c1cdd2ed9a86d6061aedb
olek1305 <olek1305@gmail.com>
2023-08-12 21:15:08 +0200

    Refactoring

--- LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
+++ LaravelLearnVue/toedoe-app/src/pages/TasksPage.vue
@@ -4,11 +4,10 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-                    <NewTask @added="handleAddedTask" />
+                    <NewTask />
 
                     <!-- List of uncompleted tasks -->
-                    <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask" @completed="handleCompletedTask"
-                        @removed="handleRemovedTask" />
+                    <Tasks :tasks="uncompletedTasks" />
 
                     <!-- show toggle button -->
                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
@@ -20,8 +19,7 @@
                     </div>
 
                     <!-- show toggle button -->
-                    <Tasks :tasks="completedTasks" :show="completedTasksVisible && showCompletedTasks"
-                        @updated="handleUpdatedTask" @completed="handleCompletedTask" @removed="handleRemovedTask" />
+                    <Tasks :tasks="completedTasks" :show="completedTasksVisible && showCompletedTasks"/>
                 </div>
             </div>
         </div>
@@ -32,15 +30,12 @@
 import { onMounted, ref, computed } from "vue";
 import { storeToRefs } from "pinia"
 import { useTaskStore } from "../stores/task";
-import { allTasks, createTask, updateTask, completeTask, removeTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue"
 import NewTask from "../components/tasks/NewTask.vue"
 
 const store = useTaskStore()
 const { completedTasks, uncompletedTasks } = storeToRefs(store)
 const { fetchAllTasks } = store
-
-const tasks = ref([])
 
 onMounted(async () => {
     await fetchAllTasks()
@@ -55,30 +50,4 @@ const completedTasksVisible = computed(
 
 const showCompletedTasks = ref(false)
 
-const handleAddedTask = async (newTask) => {
-    const { data: createdTask } = await createTask(newTask)
-    tasks.value.unshift(createdTask.data)
-}
-
-const handleUpdatedTask = async (task) => {
-    const { data: updatedTask } = await updateTask(task.id, {
-        name: task.name
-    })
-    const currentTask = tasks.value.find(item => item.id === task.id)
-    currentTask.name = updatedTask.data.name
-}
-
-const handleCompletedTask = async (task) => {
-    const { data: updatedTask } = await completeTask(task.id, {
-        is_completed: task.is_completed
-    })
-    const currentTask = tasks.value.find(item => item.id === task.id)
-    currentTask.is_completed = updatedTask.data.is_completed
-}
-
-const handleRemovedTask = async (task) => {
-    await removeTask(task.id)
-    const index = tasks.value.findIndex(item => item.id === task.id)
-    tasks.value.splice(index, 1)
-}
 </script>
--- LaravelLearnVue/toedoe-app/src/stores/task.js
+++ LaravelLearnVue/toedoe-app/src/stores/task.js
@@ -1,19 +1,6 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
-import { allTasks } from "../http/task-api"
-
-const tmp = {
-    state: () => ({
-
-    }),
-    getters: {
-
-    },
-
-    actions: {
-
-    }
-}
+import { allTasks, createTask, completeTask, removeTask, updateTask } from "../http/task-api"
 
 export const useTaskStore = defineStore('taskStore', () => {
     const tasks = ref([])
@@ -24,9 +11,36 @@ export const useTaskStore = defineStore('taskStore', () => {
     const fetchAllTasks = async () => {
         const { data } = await allTasks()
         tasks.value = data.data
-    }
+    };
+
+    const handleAddedTask = async (newTask) => {
+        const { data: createdTask } = await createTask(newTask)
+        tasks.value.unshift(createdTask.data)
+    };
+
+    const handleUpdatedTask = async (task) => {
+        const { data: updatedTask } = await updateTask(task.id, {
+            name: task.name
+        })
+        const currentTask = tasks.value.find(item => item.id === task.id)
+        currentTask.name = updatedTask.data.name
+    };
+
+    const handleCompletedTask = async (task) => {
+        const { data: updatedTask } = await completeTask(task.id, {
+            is_completed: task.is_completed
+        })
+        const currentTask = tasks.value.find(item => item.id === task.id)
+        currentTask.is_completed = updatedTask.data.is_completed
+    };
+
+    const handleRemovedTask = async (task) => {
+        await removeTask(task.id)
+        const index = tasks.value.findIndex(item => item.id === task.id)
+        tasks.value.splice(index, 1)
+    };
 
     return {
-        tasks, completedTasks, uncompletedTasks, fetchAllTasks
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks, handleAddedTask, handleUpdatedTask, handleCompletedTask, handleRemovedTask
     }
 });
