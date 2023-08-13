71352b1aa90c016e6279ddf887dd8ae7a310933d
olek1305 <olek1305@gmail.com>
2023-08-14 01:35:49 +0200

    add page summary and   dynamic active link

--- LaravelLearnVue/toedoe-app/src/components/Navbar.vue
+++ LaravelLearnVue/toedoe-app/src/components/Navbar.vue
@@ -12,11 +12,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <!-- <router-link :to="{ name: 'tasks'}" class="nav-link">Tasks</router-link> -->
-                        <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Tasks</a>
+                        <router-link :to="{ name: 'tasks'}" class="nav-link">Tasks</router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Summary</a>
+                        <router-link :to="{ name: 'summary' }" class="nav-link">Summary</router-link>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
@@ -34,3 +33,9 @@
         </div>
     </nav>
 </template>
+
+<!-- <style scoped>
+    .nav-link.router-link-active {
+        color: rgba(0, 0, 0, .9);
+    }
+</style> -->
