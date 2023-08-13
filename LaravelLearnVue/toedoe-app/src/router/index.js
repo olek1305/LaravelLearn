a86da9d0f46891e7c82898b1635a97020fed0d07
olek1305 <olek1305@gmail.com>
2023-08-14 01:57:25 +0200

    Navigation Guards

--- LaravelLearnVue/toedoe-app/src/router/index.js
+++ LaravelLearnVue/toedoe-app/src/router/index.js
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
 
-export default createRouter({
+const router = createRouter({
     routes, 
     history: createWebHistory(),
     linkActiveClass: "active",
 })
+
+router.beforeEach((to, from) => {
+    console.log("Global before each", to, from);
+    if (to.path === '/tasks' ) { 
+        return { name: "login" };
+    }
+})
+
+export default router
