f55d7570ed2287c96f0979ee4ec0b29133fc2e0b
olek1305 <olek1305@gmail.com>
2023-08-15 15:46:16 +0200

    change route link

--- LaravelLearnVue/toedoe-app/src/components/Navbar.vue
+++ LaravelLearnVue/toedoe-app/src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <div class="container py-2">
-            <a href="#" class="navbar-brand">
+            <router-link :to="{ name: 'home' }" class="navbar-brand">
                 <span>Toedoe</span>
                 <strong>List</strong>
-            </a>
+            </router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <router-link :to="{ name: 'tasks'}" class="nav-link">Tasks</router-link>
+                        <router-link :to="{ name: 'tasks' }" class="nav-link">Tasks</router-link>
                     </li>
                     <li class="nav-item">
                         <router-link :to="{ name: 'summary' }" class="nav-link">Summary</router-link>
@@ -20,10 +20,10 @@
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <router-link :to="{ name: 'login'}" class="btn btn-outline-secondary ms-2">Login</router-link>
+                        <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2">Login</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link :to="{ name: 'register'}" class="btn btn-danger ms-2">Register</router-link>
+                        <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
