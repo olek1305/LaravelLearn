f51073e5dc15871de64649cbfe33db72864c031c
olek1305 <olek1305@gmail.com>
2023-08-21 18:34:54 +0200

    login page

--- LaravelLearnVue/toedoe-app/src/http/task-api.js
+++ LaravelLearnVue/toedoe-app/src/http/task-api.js
@@ -1,7 +1,7 @@
-import api from "./api"
+import api from "./api";
 
 const apiPath = import.meta.env.VITE_API_PATH;
-const resource = apiPath + "/tasks"
+const resource = apiPath + "/tasks";
 
 export const allTasks = () => api.get(resource);
 
@@ -12,4 +12,4 @@ export const updateTask = (id, task) => api.put(`${resource}/${id}`, task);
 export const removeTask = (id) => api.delete(`${resource}/${id}`);
 
 export const completeTask = (id, task) =>
-    api.patch(`${resource}/${id}/complete`, task);
+  api.patch(`${resource}/${id}/complete`, task);
