2ee8ad1fb5d88ab5a575de4e4445bb78f4568ea6
olek1305 <olek1305@gmail.com>
2023-08-22 23:33:39 +0200

    fix api

--- LaravelLearnVue/toedoe-app/src/http/auth-api.js
+++ LaravelLearnVue/toedoe-app/src/http/auth-api.js
@@ -1,11 +1,11 @@
 import api from "./api";
 
-export const csrfCookie = () => api.get('/sanctum/csrf-cookie');
+export const csrfCookie = () => api.get("/sanctum/csrf-cookie");
 
-export const login = (credentials) => api.post('/auth/login', credentials);
+export const login = (credentials) => api.post("/auth/login", credentials);
 
-export const register = (user) => api.post('/auth/register', user);
+export const register = (user) => api.post("/auth/register", user);
 
-export const logout = () => api.post('/auth/logout');
+export const logout = () => api.post("/auth/logout");
 
-export const getUser = () => api.get('/api/user');
+export const getUser = () => api.get("/api/user");
