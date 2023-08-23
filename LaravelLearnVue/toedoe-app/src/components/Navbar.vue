96157e4ae21680a21614da4cc315cc1ab8d14dfd
olek1305 <olek1305@gmail.com>
2023-08-23 22:42:05 +0200

    add dropdown

--- LaravelLearnVue/toedoe-app/src/components/Navbar.vue
+++ LaravelLearnVue/toedoe-app/src/components/Navbar.vue
@@ -21,15 +21,34 @@
                 <ul class="navbar-nav ms-auto">
                     <template v-if="!store.isLoggedIn">
                         <li class="nav-item">
-                        <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2">Login</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
-                    </li>
+                            <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2">Login</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
+                        </li>
                     </template>
                     <template v-else>
-                        <li class="nav-item">
-                        <a href="#" class="btn btn-outline-secondary ms-2" @click.prevent="logout">Logout</a>
+                        <!--  -->
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" :class="toggleClass" @click.prevent="toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                {{ store.user.name }}
+                            </a>
+                            <ul class="dropdown-menu" :class="toggleClass">
+                                <li class="nav-item">
+                                    <a href="#" class="dropdown-item" @click.prevent="logout">Logout</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Example</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider" />
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Example</a>
+                                </li>
+                            </ul>
                         </li>
                     </template>
                 </ul>
@@ -39,20 +58,27 @@
 </template>
 
 <script setup>
-    import { useRouter } from "vue-router";
-    import { useAuthStore } from "../stores/auth";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+import { ref, computed } from "vue";
 
-    const router = useRouter ()
-    const store = useAuthStore ()
+const router = useRouter();
+const store = useAuthStore();
+const isOpen = ref(false)
 
-    const logout = async () => {
-        await store.handleLogout()
-        router.push({ name: 'login' })
-    }
+const logout = async () => {
+    await store.handleLogout()
+    isOpen.value = false
+    router.push({ name: "login" });
+}
+
+const toggle = () => isOpen.value = !isOpen.value
+
+const toggleClass = computed(() => isOpen.value === true ? 'show' : '')
 </script>
 
 <style scoped>
-    .nav-link.router-link-active {
-        color: rgba(0, 0, 0, .9);
-    }
+.nav-link.router-link-active {
+    color: rgba(0, 0, 0, 0.9);
+}
 </style>
